fcec671977806b0409875d20f1a047e336e8bb00
ggojkovic <gojkovic.goran73@gmail.com>
2025-02-04 14:26:09 +0100

    changed config

--- src/main/java/com/ggcode/ggshopify/GGShopifyApplication.java
+++ src/main/java/com/ggcode/ggshopify/GGShopifyApplication.java
@@ -1,18 +1,15 @@
 package com.ggcode.ggshopify;
 
-import com.ggcode.ggshopify.config.ShopifyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ShopifyConfig.class)
 @ComponentScan(basePackages = {"com.ggcode.ggshopify"})
 public class GGShopifyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GGShopifyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GGShopifyApplication.class, args);
+    }
 
 }
